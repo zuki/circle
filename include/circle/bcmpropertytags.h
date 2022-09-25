0be5e9dac0d7e7fb1aedac46fc4ac3896f81c40e
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 14:59:58 +0900

    bcmpropertytagsのコードのドキュメント付け

--- include/circle/bcmpropertytags.h
+++ include/circle/bcmpropertytags.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2021  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -16,7 +16,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 #ifndef _circle_bcmpropertytags_h
 #define _circle_bcmpropertytags_h
 
@@ -24,275 +24,359 @@
 #include <circle/macros.h>
 #include <circle/types.h>
 
-#define PROPTAG_END			0x00000000
+// MailBoxタグ
 
-#define PROPTAG_GET_FIRMWARE_REVISION	0x00000001
-#define PROPTAG_SET_CURSOR_INFO		0x00008010
-#define PROPTAG_SET_CURSOR_STATE	0x00008011
-#define PROPTAG_GET_BOARD_MODEL		0x00010001
-#define PROPTAG_GET_BOARD_REVISION	0x00010002
-#define PROPTAG_GET_MAC_ADDRESS		0x00010003
-#define PROPTAG_GET_BOARD_SERIAL	0x00010004
-#define PROPTAG_GET_ARM_MEMORY		0x00010005
-#define PROPTAG_GET_VC_MEMORY		0x00010006
-#define PROPTAG_SET_POWER_STATE		0x00028001
-#define PROPTAG_GET_CLOCK_RATE		0x00030002
-#define PROPTAG_GET_MAX_CLOCK_RATE	0x00030004
-#define PROPTAG_GET_TEMPERATURE		0x00030006
-#define PROPTAG_GET_MIN_CLOCK_RATE	0x00030007
-#define PROPTAG_GET_TURBO		0x00030009
-#define PROPTAG_GET_MAX_TEMPERATURE	0x0003000A
-#define PROPTAG_GET_EDID_BLOCK		0x00030020
-#define PROPTAG_GET_THROTTLED		0x00030046
-#define PROPTAG_GET_CLOCK_RATE_MEASURED	0x00030047
-#define PROPTAG_NOTIFY_XHCI_RESET	0x00030058
-#define PROPTAG_SET_CLOCK_RATE		0x00038002
-#define PROPTAG_SET_TURBO		0x00038009
-#define PROPTAG_SET_SET_GPIO_STATE	0x00038041
-#define PROPTAG_SET_SDHOST_CLOCK	0x00038042
-#define PROPTAG_ALLOCATE_BUFFER		0x00040001
-#define PROPTAG_GET_DISPLAY_DIMENSIONS	0x00040003
-#define PROPTAG_GET_PITCH		0x00040008
-#define PROPTAG_GET_TOUCHBUF		0x0004000F
-#define PROPTAG_GET_GPIO_VIRTBUF	0x00040010
-#define PROPTAG_GET_NUM_DISPLAYS	0x00040013
-#define PROPTAG_SET_PHYS_WIDTH_HEIGHT	0x00048003
-#define PROPTAG_SET_VIRT_WIDTH_HEIGHT	0x00048004
-#define PROPTAG_SET_DEPTH		0x00048005
-#define PROPTAG_SET_VIRTUAL_OFFSET	0x00048009
-#define PROPTAG_SET_PALETTE		0x0004800B
-#define PROPTAG_WAIT_FOR_VSYNC		0x0004800E
-#define PROPTAG_SET_BACKLIGHT		0x0004800F
-#define PROPTAG_SET_DISPLAY_NUM		0x00048013
-#define PROPTAG_SET_TOUCHBUF		0x0004801F
-#define PROPTAG_SET_GPIO_VIRTBUF	0x00048020
-#define PROPTAG_GET_COMMAND_LINE	0x00050001
-#define PROPTAG_GET_DMA_CHANNELS	0x00060001
+#define PROPTAG_END                     0x00000000
 
+#define PROPTAG_GET_FIRMWARE_REVISION   0x00000001
+#define PROPTAG_SET_CURSOR_INFO         0x00008010
+#define PROPTAG_SET_CURSOR_STATE        0x00008011
+#define PROPTAG_GET_BOARD_MODEL         0x00010001
+#define PROPTAG_GET_BOARD_REVISION      0x00010002
+#define PROPTAG_GET_MAC_ADDRESS         0x00010003
+#define PROPTAG_GET_BOARD_SERIAL        0x00010004
+#define PROPTAG_GET_ARM_MEMORY          0x00010005
+#define PROPTAG_GET_VC_MEMORY           0x00010006
+#define PROPTAG_SET_POWER_STATE         0x00028001
+#define PROPTAG_GET_CLOCK_RATE          0x00030002
+#define PROPTAG_GET_MAX_CLOCK_RATE      0x00030004
+#define PROPTAG_GET_TEMPERATURE         0x00030006
+#define PROPTAG_GET_MIN_CLOCK_RATE      0x00030007
+#define PROPTAG_GET_TURBO               0x00030009
+#define PROPTAG_GET_MAX_TEMPERATURE     0x0003000A
+#define PROPTAG_GET_EDID_BLOCK          0x00030020
+#define PROPTAG_GET_THROTTLED           0x00030046
+#define PROPTAG_GET_CLOCK_RATE_MEASURED 0x00030047
+#define PROPTAG_NOTIFY_XHCI_RESET       0x00030058
+#define PROPTAG_SET_CLOCK_RATE          0x00038002
+#define PROPTAG_SET_TURBO               0x00038009
+#define PROPTAG_SET_SET_GPIO_STATE      0x00038041
+#define PROPTAG_SET_SDHOST_CLOCK        0x00038042
+#define PROPTAG_ALLOCATE_BUFFER         0x00040001
+#define PROPTAG_GET_DISPLAY_DIMENSIONS  0x00040003
+#define PROPTAG_GET_PITCH               0x00040008
+#define PROPTAG_GET_TOUCHBUF            0x0004000F
+#define PROPTAG_GET_GPIO_VIRTBUF        0x00040010
+#define PROPTAG_GET_NUM_DISPLAYS        0x00040013
+#define PROPTAG_SET_PHYS_WIDTH_HEIGHT   0x00048003
+#define PROPTAG_SET_VIRT_WIDTH_HEIGHT   0x00048004
+#define PROPTAG_SET_DEPTH               0x00048005
+#define PROPTAG_SET_VIRTUAL_OFFSET      0x00048009
+#define PROPTAG_SET_PALETTE             0x0004800B
+#define PROPTAG_WAIT_FOR_VSYNC          0x0004800E
+#define PROPTAG_SET_BACKLIGHT           0x0004800F
+#define PROPTAG_SET_DISPLAY_NUM         0x00048013
+#define PROPTAG_SET_TOUCHBUF            0x0004801F
+#define PROPTAG_SET_GPIO_VIRTBUF        0x00048020
+#define PROPTAG_GET_COMMAND_LINE        0x00050001
+#define PROPTAG_GET_DMA_CHANNELS        0x00060001
+
+//! MailBox属性タグ
+
+/**
+ * @struct TPropertyTag
+ * @brief 属性タグ共通部分を表す構造体
+ */
 struct TPropertyTag
 {
-	u32	nTagId;
-	u32	nValueBufSize;			// bytes, multiple of 4
-	u32	nValueLength;			// bytes
-	#define VALUE_LENGTH_RESPONSE	(1 << 31)
-	//u8	ValueBuffer[0];			// must be padded to be 4 byte aligned
+    u32     nTagId;
+    u32     nValueBufSize;          // bytes, multiple of 4
+    u32     nValueLength;           // bytes
+    #define VALUE_LENGTH_RESPONSE   (1 << 31)
+    //u8    ValueBuffer[0];         // must be padded to be 4 byte aligned
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSimple
+ * @brief リクエスト属性を持たない単純な属性タグを表す構造体
+ */
 struct TPropertyTagSimple
 {
-	TPropertyTag	Tag;
-	u32		nValue;
+    TPropertyTag    Tag;
+    u32             nValue;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSetCursorInfo
+ * @brief カーソル情報の設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagSetCursorInfo
 {
-	TPropertyTag	Tag;
-	union
-	{
-		u32	nWidth;			// should be >= 16
-		u32	nResponse;
-	#define CURSOR_RESPONSE_VALID	0	// response
-	}
-	PACKED;
-	u32		nHeight;		// should be >= 16
-	u32		nUnused;
-	u32		nPixelPointer;		// physical address, format 32bpp ARGB
-	u32		nHotspotX;
-	u32		nHotspotY;
+    TPropertyTag    Tag;
+    union
+    {
+        u32    nWidth;                  // should be >= 16
+        u32    nResponse;
+    #define CURSOR_RESPONSE_VALID   0   // response
+    }
+    PACKED;
+    u32        nHeight;                 // should be >= 16
+    u32        nUnused;
+    u32        nPixelPointer;           // physical address, format 32bpp ARGB
+    u32        nHotspotX;
+    u32        nHotspotY;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSetCursorState
+ * @brief カーソル状態の設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagSetCursorState
 {
-	TPropertyTag	Tag;
-	union
-	{
-		u32	nEnable;
-	#define CURSOR_ENABLE_INVISIBLE		0
-	#define CURSOR_ENABLE_VISIBLE		1
-		u32	nResponse;
-	}
-	PACKED;
-	u32		nPosX;
-	u32		nPosY;
-	u32		nFlags;
-	#define CURSOR_FLAGS_DISP_COORDS	0
-	#define CURSOR_FLAGS_FB_COORDS		1
+    TPropertyTag    Tag;
+    union
+    {
+        u32    nEnable;
+    #define CURSOR_ENABLE_INVISIBLE     0
+    #define CURSOR_ENABLE_VISIBLE       1
+        u32    nResponse;
+    }
+    PACKED;
+    u32        nPosX;
+    u32        nPosY;
+    u32        nFlags;
+    #define CURSOR_FLAGS_DISP_COORDS    0
+    #define CURSOR_FLAGS_FB_COORDS      1
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagMACAddress
+ * @brief MACアドレスの取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagMACAddress
 {
-	TPropertyTag	Tag;
-	u8		Address[6];
-	u8		Padding[2];
+    TPropertyTag    Tag;
+    u8              Address[6];
+    u8              Padding[2];
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSerial
+ * @brief シリアルの取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagSerial
 {
-	TPropertyTag	Tag;
-	u32		Serial[2];
+    TPropertyTag    Tag;
+    u32             Serial[2];
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagMemory
+ * @brief メモリの取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagMemory
 {
-	TPropertyTag	Tag;
-	u32		nBaseAddress;
-	u32		nSize;			// bytes
+    TPropertyTag    Tag;
+    u32             nBaseAddress;
+    u32             nSize;            // bytes
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagPowerState
+ * @brief 電源状態の設定/取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagPowerState
 {
-	TPropertyTag	Tag;
-	u32		nDeviceId;
-	#define DEVICE_ID_SD_CARD	0
-	#define DEVICE_ID_USB_HCD	3
-	u32		nState;
-	#define POWER_STATE_OFF		(0 << 0)
-	#define POWER_STATE_ON		(1 << 0)
-	#define POWER_STATE_WAIT	(1 << 1)
-	#define POWER_STATE_NO_DEVICE	(1 << 1)	// in response
+    TPropertyTag    Tag;
+    u32             nDeviceId;
+    #define DEVICE_ID_SD_CARD   0
+    #define DEVICE_ID_USB_HCD   3
+    u32             nState;
+    #define POWER_STATE_OFF         (0 << 0)
+    #define POWER_STATE_ON          (1 << 0)
+    #define POWER_STATE_WAIT        (1 << 1)
+    #define POWER_STATE_NO_DEVICE   (1 << 1)    // in response
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagClockRate
+ * @brief クロックレートの取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagClockRate
 {
-	TPropertyTag	Tag;
-	u32		nClockId;
-	#define CLOCK_ID_EMMC		1
-	#define CLOCK_ID_UART		2
-	#define CLOCK_ID_ARM		3
-	#define CLOCK_ID_CORE		4
-	#define CLOCK_ID_EMMC2		12
-	#define CLOCK_ID_PIXEL_BVB	14
-	u32		nRate;			// Hz
+    TPropertyTag    Tag;
+    u32             nClockId;
+    #define CLOCK_ID_EMMC       1
+    #define CLOCK_ID_UART       2
+    #define CLOCK_ID_ARM        3
+    #define CLOCK_ID_CORE       4
+    #define CLOCK_ID_EMMC2      12
+    #define CLOCK_ID_PIXEL_BVB  14
+    u32             nRate;           // Hz
 }
 PACKED;
 
+/**
+ * @struct TPropertyTemperature
+ * @brief 温度の取得を行う属性タグを表す構造体
+ */
 struct TPropertyTemperature
 {
-	TPropertyTag	Tag;
-	u32		nTemperatureId;
-	#define TEMPERATURE_ID		0
-	u32		nValue;			// degree Celsius * 1000
+    TPropertyTag    Tag;
+    u32             nTemperatureId;
+    #define TEMPERATURE_ID      0
+    u32             nValue;         // degree Celsius * 1000
 }
 PACKED;
-#define TPropertyTagTemperature		TPropertyTemperature
+#define TPropertyTagTemperature        TPropertyTemperature
 
+/**
+ * @struct TPropertyTagTurbo
+ * @brief ターボの設定/取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagTurbo
 {
-	TPropertyTag	Tag;
-	u32		nTurboId;
-	#define TURBO_ID		0
-	u32		nLevel;
-	#define TURBO_OFF		0
-	#define TURBO_ON		1
+    TPropertyTag    Tag;
+    u32             nTurboId;
+    #define TURBO_ID        0
+    u32             nLevel;
+    #define TURBO_OFF       0
+    #define TURBO_ON        1
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagGPIOState
+ * @brief GPIO状態の取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagGPIOState
 {
-	TPropertyTag	Tag;
-	u32		nGPIO;
-	#define EXP_GPIO_BASE		128
-	#define EXP_GPIO_NUM		8
-	u32		nState;
+    TPropertyTag    Tag;
+    u32             nGPIO;
+    #define EXP_GPIO_BASE       128
+    #define EXP_GPIO_NUM        8
+    u32             nState;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagEDIDBlock
+ * @brief EDIDブロックの取得を行う属性タグを表す構造体（HDMI/DVI機器）
+ */
 struct TPropertyTagEDIDBlock
 {
-	TPropertyTag	Tag;
-	u32		nBlockNumber;
-	#define EDID_FIRST_BLOCK	0
-	u32		nStatus;
-	#define EDID_STATUS_SUCCESS	0
-	u8		Block[128];
+    TPropertyTag    Tag;
+    u32             nBlockNumber;
+    #define EDID_FIRST_BLOCK        0
+    u32             nStatus;
+    #define EDID_STATUS_SUCCESS     0
+    u8              Block[128];
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSetClockRate
+ * @brief クロックレートの設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagSetClockRate
 {
-	TPropertyTag	Tag;
-	u32		nClockId;
-	u32		nRate;			// Hz
-	u32		nSkipSettingTurbo;
-	#define SKIP_SETTING_TURBO	1	// when setting ARM clock
+    TPropertyTag    Tag;
+    u32             nClockId;
+    u32             nRate;              // Hz
+    u32             nSkipSettingTurbo;
+    #define SKIP_SETTING_TURBO      1   // when setting ARM clock
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagAllocateBuffer
+ * @brief バッファの割当を行う属性タグを表す構造体
+ */
 struct TPropertyTagAllocateBuffer
 {
-	TPropertyTag	Tag;
-	union
-	{
-		u32	nAlignment;		// in bytes
-		u32	nBufferBaseAddress;
-	}
-	PACKED;
-	u32		nBufferSize;
+    TPropertyTag    Tag;
+    union
+    {
+        u32         nAlignment;         // バイト単位（リクエスト）
+        u32         nBufferBaseAddress; // （レスポンス）
+    }
+    PACKED;
+    u32             nBufferSize;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagDisplayDimensions
+ * @brief ディスプレイの幅/高さの設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagDisplayDimensions
 {
-	TPropertyTag	Tag;
-	u32		nWidth;
-	u32		nHeight;
+    TPropertyTag    Tag;
+    u32             nWidth;
+    u32             nHeight;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagVirtualOffset
+ * @brief 仮想（バッファ）の幅/高さの設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagVirtualOffset
 {
-	TPropertyTag	Tag;
-	u32		nOffsetX;
-	u32		nOffsetY;
+    TPropertyTag    Tag;
+    u32             nOffsetX;
+    u32             nOffsetY;
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagSetPalette
+ * @brief パレットの取得/設定を行う属性タグを表す構造体
+ */
 struct TPropertyTagSetPalette
 {
-	TPropertyTag	Tag;
-	union
-	{
-		u32	nOffset;		// first palette index to set (0-255)
-		u32	nResult;
-	#define SET_PALETTE_VALID	0
-	}
-	PACKED;
-	u32		nLength;		// number of palette entries to set (1-256)
-	u32		Palette[0];		// RGBA values, offset to offset+length-1
+    TPropertyTag    Tag;
+    union
+    {
+        u32         nOffset;         // セットする最初のパレットインデックス（0-255）
+        u32         nResult;
+    #define SET_PALETTE_VALID   0
+    }
+    PACKED;
+    u32             nLength;         // 設定するパレットエントリの数（1-256）
+    u32             Palette[0];      // RGBAパレット値（オフセット - オフセット+長さ-1）
 }
 PACKED;
 
+/**
+ * @struct TPropertyTagCommandLine
+ * @brief コマンドラインの取得を行う属性タグを表す構造体
+ */
 struct TPropertyTagCommandLine
 {
-	TPropertyTag	Tag;
-	u8		String[2048];
+    TPropertyTag    Tag;
+    u8              String[2048];
 }
 PACKED;
 
+/**
+ * @class
+ * @brief Mailbox属性タグを表すクラス
+ */
 class CBcmPropertyTags
 {
 public:
-	CBcmPropertyTags (boolean bEarlyUse = FALSE);		// do not use spinlock early
-	~CBcmPropertyTags (void);
+    CBcmPropertyTags (boolean bEarlyUse = FALSE);   // 初期にスピンロックを使用しない
+    ~CBcmPropertyTags (void);
 
-	boolean GetTag (u32	  nTagId,			// tag identifier
-			void	 *pTag,				// pointer to tag struct
-			unsigned  nTagSize,			// size of tag struct
-			unsigned  nRequestParmSize = 0);	// number of parameter bytes
-	
-	boolean GetTags (void	 *pTags,			// pointer to tags struct
-			 unsigned nTagsSize);			// size of tags struct
+    boolean GetTag (u32 nTagId,                     //< タグID
+            void       *pTag,                       //< タグ構造体へのポインタ
+            unsigned    nTagSize,                   //< タグ構造体のサイズ
+            unsigned    nRequestParmSize = 0);      //< パラメタバイト数
+
+    boolean GetTags (void  *pTags,                  //< タグ構造体へのポインタ
+             unsigned       nTagsSize);             //< タグ構造体のサイズ
 
 private:
-	CBcmMailBox m_MailBox;
+    CBcmMailBox m_MailBox;
 };
 
 #endif
--- lib/bcmpropertytags.cpp
+++ lib/bcmpropertytags.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2019  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -16,7 +16,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 #include <circle/bcmpropertytags.h>
 #include <circle/util.h>
 #include <circle/synchronize.h>
@@ -27,18 +27,18 @@
 
 struct TPropertyBuffer
 {
-	u32	nBufferSize;			// bytes
-	u32	nCode;
-	#define CODE_REQUEST		0x00000000
-	#define CODE_RESPONSE_SUCCESS	0x80000000
-	#define CODE_RESPONSE_FAILURE	0x80000001
-	u8	Tags[0];
-	// end tag follows
+    u32     nBufferSize;            // bytes
+    u32     nCode;
+    #define CODE_REQUEST            0x00000000
+    #define CODE_RESPONSE_SUCCESS   0x80000000
+    #define CODE_RESPONSE_FAILURE   0x80000001
+    u8      Tags[0];
+    // end tag follows
 }
 PACKED;
 
 CBcmPropertyTags::CBcmPropertyTags (boolean bEarlyUse)
-:	m_MailBox (BCM_MAILBOX_PROP_OUT, bEarlyUse)
+:    m_MailBox (BCM_MAILBOX_PROP_OUT, bEarlyUse)
 {
 }
 
@@ -48,61 +48,61 @@ CBcmPropertyTags::~CBcmPropertyTags (void)
 
 boolean CBcmPropertyTags::GetTag (u32 nTagId, void *pTag, unsigned nTagSize, unsigned nRequestParmSize)
 {
-	assert (pTag != 0);
-	assert (nTagSize >= sizeof (TPropertyTagSimple));
+    assert (pTag != 0);
+    assert (nTagSize >= sizeof (TPropertyTagSimple));
 
-	TPropertyTag *pHeader = (TPropertyTag *) pTag;
-	pHeader->nTagId = nTagId;
-	pHeader->nValueBufSize = nTagSize - sizeof (TPropertyTag);
-	pHeader->nValueLength = nRequestParmSize & ~VALUE_LENGTH_RESPONSE;
+    TPropertyTag *pHeader = (TPropertyTag *) pTag;
+    pHeader->nTagId = nTagId;
+    pHeader->nValueBufSize = nTagSize - sizeof (TPropertyTag);
+    pHeader->nValueLength = nRequestParmSize & ~VALUE_LENGTH_RESPONSE;
 
-	if (!GetTags (pTag, nTagSize))
-	{
-		return FALSE;
-	}
+    if (!GetTags (pTag, nTagSize))
+    {
+        return FALSE;
+    }
 
-	pHeader->nValueLength &= ~VALUE_LENGTH_RESPONSE;
-	if (pHeader->nValueLength == 0)
-	{
-		return FALSE;
-	}
+    pHeader->nValueLength &= ~VALUE_LENGTH_RESPONSE;
+    if (pHeader->nValueLength == 0)
+    {
+        return FALSE;
+    }
 
-	return TRUE;
+    return TRUE;
 }
 
 boolean CBcmPropertyTags::GetTags (void *pTags, unsigned nTagsSize)
 {
-	assert (pTags != 0);
-	assert (nTagsSize >= sizeof (TPropertyTagSimple));
-	unsigned nBufferSize = sizeof (TPropertyBuffer) + nTagsSize + sizeof (u32);
-	assert ((nBufferSize & 3) == 0);
+    assert (pTags != 0);
+    assert (nTagsSize >= sizeof (TPropertyTagSimple));
+    unsigned nBufferSize = sizeof (TPropertyBuffer) + nTagsSize + sizeof (u32);
+    assert ((nBufferSize & 3) == 0);
 
-	TPropertyBuffer *pBuffer =
-		(TPropertyBuffer *) CMemorySystem::GetCoherentPage (COHERENT_SLOT_PROP_MAILBOX);
+    TPropertyBuffer *pBuffer =
+        (TPropertyBuffer *) CMemorySystem::GetCoherentPage (COHERENT_SLOT_PROP_MAILBOX);
 
-	pBuffer->nBufferSize = nBufferSize;
-	pBuffer->nCode = CODE_REQUEST;
-	memcpy (pBuffer->Tags, pTags, nTagsSize);
+    pBuffer->nBufferSize = nBufferSize;
+    pBuffer->nCode = CODE_REQUEST;
+    memcpy (pBuffer->Tags, pTags, nTagsSize);
 
-	u32 *pEndTag = (u32 *) (pBuffer->Tags + nTagsSize);
-	*pEndTag = PROPTAG_END;
+    u32 *pEndTag = (u32 *) (pBuffer->Tags + nTagsSize);
+    *pEndTag = PROPTAG_END;
 
-	DataSyncBarrier ();
+    DataSyncBarrier ();
 
-	u32 nBufferAddress = BUS_ADDRESS ((uintptr) pBuffer);
-	if (m_MailBox.WriteRead (nBufferAddress) != nBufferAddress)
-	{
-		return FALSE;
-	}
+    u32 nBufferAddress = BUS_ADDRESS ((uintptr) pBuffer);
+    if (m_MailBox.WriteRead (nBufferAddress) != nBufferAddress)
+    {
+        return FALSE;
+    }
 
-	DataMemBarrier ();
+    DataMemBarrier ();
 
-	if (pBuffer->nCode != CODE_RESPONSE_SUCCESS)
-	{
-		return FALSE;
-	}
+    if (pBuffer->nCode != CODE_RESPONSE_SUCCESS)
+    {
+        return FALSE;
+    }
 
-	memcpy (pTags, pBuffer->Tags, nTagsSize);
+    memcpy (pTags, pBuffer->Tags, nTagsSize);
 
-	return TRUE;
+    return TRUE;
 }
