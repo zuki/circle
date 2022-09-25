a583458f9ab9d3ed39393ed65187f9e76ee430cb
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 15:06:37 +0900

    net/netdevlayerのコードのドキュメント付け

--- include/circle/net/netdevlayer.h
+++ include/circle/net/netdevlayer.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -26,34 +26,53 @@
 #include <circle/bcm54213.h>
 #include <circle/types.h>
 
+/// @brief 物理層（ネットワークデバイス）を表すクラス
 class CNetDeviceLayer
 {
 public:
-	CNetDeviceLayer (CNetConfig *pNetConfig, TNetDeviceType DeviceType);
-	~CNetDeviceLayer (void);
+    /// @brief コンストラクタ
+    /// @param pNetConfig ネットワーク構成オブジェクトへのポインタ
+    /// @param DeviceType ネットワークデバイスタイプ
+    CNetDeviceLayer (CNetConfig *pNetConfig, TNetDeviceType DeviceType);
+    /// @brief デストラクタ
+    ~CNetDeviceLayer (void);
+    /// @brief 初期化関数（bWaitForActivate=FALSEの場合は直ちにTRUEで返る）
+    /// @param bWaitForActivate デバイスの活性化を待つか
+    /// @return 初期化の成否
+    boolean Initialize (boolean bWaitForActivate);
+    /// @brief 送受信を行う
+    void Process (void);
 
-	boolean Initialize (boolean bWaitForActivate);
-
-	void Process (void);
-
-	// returns 0, if net device is not available yet
-	const CMACAddress *GetMACAddress (void) const;
-
-	void Send (const void *pBuffer, unsigned nLength);
-	boolean Receive (void *pBuffer, unsigned *pResultLength);
-
-	boolean IsRunning (void) const;			// is net device available?
+    /// @brief デバイスのMACアドレスを取得する
+    /// @return デバイスのMACアドレス、デバイスが設定されていない場合は0
+    const CMACAddress *GetMACAddress (void) const;
+    /// @brief 送信キューにデータを追加する
+    /// @param pBuffer データバッファ
+    /// @param nLength データ長
+    void Send (const void *pBuffer, unsigned nLength);
+    /// @brief 受信キューからデータを取得する
+    /// @param pBuffer 受信バッファ
+    /// @param pResultLength データ長
+    /// @return 操作の成否
+    boolean Receive (void *pBuffer, unsigned *pResultLength);
+    /// @brief デバイスが利用可能か
+    /// @return 可能であればTRUE
+    boolean IsRunning (void) const;
 
 private:
-	TNetDeviceType m_DeviceType;
-	CNetConfig *m_pNetConfig;
-	CNetDevice *m_pDevice;
-
-	CNetQueue m_TxQueue;
-	CNetQueue m_RxQueue;
+    /// @brief デバイスタイプ
+    TNetDeviceType m_DeviceType;
+    /// @brief ネットワーク構成パラメタ
+    CNetConfig *m_pNetConfig;
+    /// @brief ネットワークデバイス
+    CNetDevice *m_pDevice;
+    /// @brief 送信キュー
+    CNetQueue m_TxQueue;
+    /// @brief 受信キュー
+    CNetQueue m_RxQueue;
 
 #if RASPPI >= 4
-	CBcm54213Device m_Bcm54213;
+    CBcm54213Device m_Bcm54213;
 #endif
 };
 
--- lib/net/netdevlayer.cpp
+++ lib/net/netdevlayer.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -28,129 +28,129 @@
 const char FromNetDev[] = "netdev";
 
 CNetDeviceLayer::CNetDeviceLayer (CNetConfig *pNetConfig, TNetDeviceType DeviceType)
-:	m_DeviceType (DeviceType),
-	m_pNetConfig (pNetConfig),
-	m_pDevice (0)
+:   m_DeviceType (DeviceType),
+    m_pNetConfig (pNetConfig),
+    m_pDevice (0)
 {
 }
 
 CNetDeviceLayer::~CNetDeviceLayer (void)
 {
-	m_pDevice = 0;
-	m_pNetConfig = 0;
+    m_pDevice = 0;
+    m_pNetConfig = 0;
 }
 
 boolean CNetDeviceLayer::Initialize (boolean bWaitForActivate)
 {
 #if RASPPI >= 4
-	if (!m_Bcm54213.Initialize ())
-	{
-		return FALSE;
-	}
+    if (!m_Bcm54213.Initialize ())
+    {
+        return FALSE;
+    }
 #endif
 
-	if (!bWaitForActivate)
-	{
-		return TRUE;
-	}
+    if (!bWaitForActivate)
+    {
+        return TRUE;
+    }
 
-	assert (m_pDevice == 0);
-	m_pDevice = CNetDevice::GetNetDevice (m_DeviceType);
-	if (m_pDevice == 0)
-	{
-		CLogger::Get ()->Write (FromNetDev, LogError, "Net device not available");
+    assert (m_pDevice == 0);
+    m_pDevice = CNetDevice::GetNetDevice (m_DeviceType);
+    if (m_pDevice == 0)
+    {
+        CLogger::Get ()->Write (FromNetDev, LogError, "Net device not available");
 
-		return FALSE;
-	}
+        return FALSE;
+    }
 
-	new CPHYTask (m_pDevice);
+    new CPHYTask (m_pDevice);
 
-	// wait for Ethernet PHY to come up
-	unsigned nStartTicks = CTimer::Get ()->GetTicks ();
-	do
-	{
-		if (CTimer::Get ()->GetTicks () - nStartTicks >= 4*HZ)
-		{
-			CLogger::Get ()->Write (FromNetDev, LogWarning, "Link is down");
+    // wait for Ethernet PHY to come up
+    unsigned nStartTicks = CTimer::Get ()->GetTicks ();
+    do
+    {
+        if (CTimer::Get ()->GetTicks () - nStartTicks >= 4*HZ)
+        {
+            CLogger::Get ()->Write (FromNetDev, LogWarning, "Link is down");
 
-			return TRUE;
-		}
-	}
-	while (!m_pDevice->IsLinkUp ());
+            return TRUE;
+        }
+    }
+    while (!m_pDevice->IsLinkUp ());
 
-	TNetDeviceSpeed Speed = m_pDevice->GetLinkSpeed ();
-	if (Speed != NetDeviceSpeedUnknown)
-	{
-		CLogger::Get ()->Write (FromNetDev, LogNotice, "Link is %s",
-					CNetDevice::GetSpeedString (Speed));
-	}
+    TNetDeviceSpeed Speed = m_pDevice->GetLinkSpeed ();
+    if (Speed != NetDeviceSpeedUnknown)
+    {
+        CLogger::Get ()->Write (FromNetDev, LogNotice, "Link is %s",
+                    CNetDevice::GetSpeedString (Speed));
+    }
 
-	return TRUE;
+    return TRUE;
 }
 
 void CNetDeviceLayer::Process (void)
 {
-	if (m_pDevice == 0)
-	{
-		m_pDevice = CNetDevice::GetNetDevice (m_DeviceType);
-		if (m_pDevice == 0)
-		{
-			return;
-		}
+    if (m_pDevice == 0)
+    {
+        m_pDevice = CNetDevice::GetNetDevice (m_DeviceType);
+        if (m_pDevice == 0)
+        {
+            return;
+        }
 
-		new CPHYTask (m_pDevice);
-	}
+        new CPHYTask (m_pDevice);
+    }
 
-	DMA_BUFFER (u8, Buffer, FRAME_BUFFER_SIZE);
-	unsigned nLength;
-	while (   m_pDevice->IsSendFrameAdvisable ()
-	       && (nLength = m_TxQueue.Dequeue (Buffer)) > 0)
-	{
-		if (!m_pDevice->SendFrame (Buffer, nLength))
-		{
-			CLogger::Get ()->Write (FromNetDev, LogWarning, "Frame dropped");
+    DMA_BUFFER (u8, Buffer, FRAME_BUFFER_SIZE);
+    unsigned nLength;
+    while (   m_pDevice->IsSendFrameAdvisable ()
+           && (nLength = m_TxQueue.Dequeue (Buffer)) > 0)
+    {
+        if (!m_pDevice->SendFrame (Buffer, nLength))
+        {
+            CLogger::Get ()->Write (FromNetDev, LogWarning, "Frame dropped");
 
-			break;
-		}
-	}
+            break;
+        }
+    }
 
-	while (m_pDevice->ReceiveFrame (Buffer, &nLength))
-	{
-		assert (nLength > 0);
-		m_RxQueue.Enqueue (Buffer, nLength);
-	}
+    while (m_pDevice->ReceiveFrame (Buffer, &nLength))
+    {
+        assert (nLength > 0);
+        m_RxQueue.Enqueue (Buffer, nLength);
+    }
 }
 
 const CMACAddress *CNetDeviceLayer::GetMACAddress (void) const
 {
-	if (m_pDevice == 0)
-	{
-		return 0;
-	}
+    if (m_pDevice == 0)
+    {
+        return 0;
+    }
 
-	return m_pDevice->GetMACAddress ();
+    return m_pDevice->GetMACAddress ();
 }
 
 void CNetDeviceLayer::Send (const void *pBuffer, unsigned nLength)
 {
-	m_TxQueue.Enqueue (pBuffer, nLength);
+    m_TxQueue.Enqueue (pBuffer, nLength);
 }
 
 boolean CNetDeviceLayer::Receive (void *pBuffer, unsigned *pResultLength)
 {
-	unsigned nLength = m_RxQueue.Dequeue (pBuffer);
-	if (nLength == 0)
-	{
-		return FALSE;
-	}
+    unsigned nLength = m_RxQueue.Dequeue (pBuffer);
+    if (nLength == 0)
+    {
+        return FALSE;
+    }
 
-	assert (pResultLength != 0);
-	*pResultLength = nLength;
+    assert (pResultLength != 0);
+    *pResultLength = nLength;
 
-	return TRUE;
+    return TRUE;
 }
 
 boolean CNetDeviceLayer::IsRunning (void) const
 {
-	return m_pDevice != 0;
+    return m_pDevice != 0;
 }
