c14199d739f7a522877acd5f6b024e6a6ec6198c
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 15:04:59 +0900

    net/linklayerのコードのドキュメント付け

--- include/circle/net/linklayer.h
+++ include/circle/net/linklayer.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -29,61 +29,100 @@
 #include <circle/macros.h>
 #include <circle/types.h>
 
+/// @brief Ethernetヘッダを表す構造体
 struct TEthernetHeader
 {
-	u8	MACReceiver[MAC_ADDRESS_SIZE];
-	u8	MACSender[MAC_ADDRESS_SIZE];
-	u16	nProtocolType;
-#define ETH_PROT_IP		0x800
-#define ETH_PROT_ARP		0x806
+    /// @brief 受信者のMACアドレス
+    u8  MACReceiver[MAC_ADDRESS_SIZE];
+    /// @brief 送信者のMACアドレス
+    u8  MACSender[MAC_ADDRESS_SIZE];
+    /// @brief プロトコルタイプ
+    u16 nProtocolType;
+#define ETH_PROT_IP     0x800
+#define ETH_PROT_ARP    0x806
 }
 PACKED;
 
 class CNetworkLayer;
 
+/// @brief データリンク層を表すクラス
 class CLinkLayer
 {
 public:
-	CLinkLayer (CNetConfig *pNetConfig, CNetDeviceLayer *pNetDevLayer);
-	~CLinkLayer (void);
+    /// @brief コンストラクタ
+    /// @param pNetConfig ネットワーク構成オブジェクトへのポインタ
+    /// @param pNetDevLayer ネットワークデバイス層オブジェクトへのポインタ
+    CLinkLayer (CNetConfig *pNetConfig, CNetDeviceLayer *pNetDevLayer);
+    /// @brief デストラクタ
+    ~CLinkLayer (void);
+    /// @brief 初期化関数(ARPハンドラの設定)
+    /// @return 常にTRUE
+    boolean Initialize (void);
 
-	boolean Initialize (void);
+    /// @brief ネットワーク層をアタッチする. \n
+    /// 通知のためにネットワーク層へのバックウェイが必要
+    /// @param pNetworkLayer ネットワーク層オブジェクトへのポインタ
+    void AttachLayer (CNetworkLayer *pNetworkLayer);
+    /// @brief データを受信し、適切な受信キューに入れる
+    void Process (void);
+    /// @brief データを送信する
+    /// @param rReceiver 宛先IPアドレス
+    /// @param pIPPacket データ
+    /// @param nLength データ長
+    /// @return 送信の成否
+    boolean Send (const CIPAddress &rReceiver, const void *pIPPacket, unsigned nLength);
 
-	// we need a back way to the network layer for notification
-	void AttachLayer (CNetworkLayer *pNetworkLayer);
-
-	void Process (void);
-
-	boolean Send (const CIPAddress &rReceiver, const void *pIPPacket, unsigned nLength);
-
-	// pBuffer must have size FRAME_BUFFER_SIZE
-	boolean Receive (void *pBuffer, unsigned *pResultLength);
+    /// @brief IPデータを受信する
+    /// @param pBuffer データを入れるバッファ（FRAME_BUFFER_SIZEのサイズが必要）
+    /// @param pResultLength 受信データ長
+    /// @return 受信データが有ればTRUE
+    boolean Receive (void *pBuffer, unsigned *pResultLength);
 
 public:
-	boolean SendRaw (const void *pFrame, unsigned nLength);
+    /// @brief フレームをそのまま送信
+    /// @param pFrame フレーム
+    /// @param nLength フレーム長
+    /// @return 常にTRUEを返す
+    boolean SendRaw (const void *pFrame, unsigned nLength);
 
-	// pBuffer must have size FRAME_BUFFER_SIZE
-	boolean ReceiveRaw (void *pBuffer, unsigned *pResultLength, CMACAddress *pSender = 0);
+    /// @brief Raw受信キューのデータを取得する
+    /// @param pBuffer 受信バッファ（FRAME_BUFFER_SIZEのサイズが必要）
+    /// @param pResultLength 受信したデータ長
+    /// @param pSender 送信元MACアドレス（不要なら0）
+    /// @return
+    boolean ReceiveRaw (void *pBuffer, unsigned *pResultLength, CMACAddress *pSender = 0);
 
-	// nProtocolType is in host byte order
-	boolean EnableReceiveRaw (u16 nProtocolType);
+    // nProtocolType is in host byte order
+
+    /// @brief Raw受信タイプをnProtocolTypeにする
+    /// @param nProtocolType プロトコルタイプ（ホストのバイトオーダ: LE）
+    /// @return すでにRaw受信タイプが設定されていた場合はFALSE
+    boolean EnableReceiveRaw (u16 nProtocolType);
 
 private:
-	// return IP packet to the network layer for notification
-	void ResolveFailed (const void *pReturnedFrame, unsigned nLength);
-	friend class CARPHandler;
+    /// @brief 送信が失敗したことをネットワーク層に通知する
+    /// @param pReturnedFrame 返されたIPパケット
+    /// @param nLength データ長
+    void ResolveFailed (const void *pReturnedFrame, unsigned nLength);
+    friend class CARPHandler;
 
 private:
-	CNetConfig *m_pNetConfig;
-	CNetDeviceLayer *m_pNetDevLayer;
-	CNetworkLayer *m_pNetworkLayer;
-	CARPHandler *m_pARPHandler;
-
-	CNetQueue m_ARPRxQueue;
-	CNetQueue m_IPRxQueue;
-
-	CNetQueue m_RawRxQueue;
-	u16 m_nRawProtocolType;
+    /// @brief ネットワーク構成パラメタオブジェクト
+    CNetConfig      *m_pNetConfig;
+    /// @brief ネットワークデバイス層オブジェクト
+    CNetDeviceLayer *m_pNetDevLayer;
+    /// @brief ネットワーク層オブジェクト
+    CNetworkLayer   *m_pNetworkLayer;
+    /// @brief APRハンドラ
+    CARPHandler     *m_pARPHandler;
+    /// @brief ARP受信キュー
+    CNetQueue       m_ARPRxQueue;
+    /// @brief IP受信キュー
+    CNetQueue       m_IPRxQueue;
+    /// @brief Raw受信キュー
+    CNetQueue       m_RawRxQueue;
+    /// @brief Rawプロトコルタイプ
+    u16             m_nRawProtocolType;
 };
 
 #endif
--- lib/net/linklayer.cpp
+++ lib/net/linklayer.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -24,212 +24,212 @@
 
 struct TRawPrivateData
 {
-	u8	MACSender[MAC_ADDRESS_SIZE];
+    u8    MACSender[MAC_ADDRESS_SIZE];
 };
 
 CLinkLayer::CLinkLayer (CNetConfig *pNetConfig, CNetDeviceLayer *pNetDevLayer)
-:	m_pNetConfig (pNetConfig),
-	m_pNetDevLayer (pNetDevLayer),
-	m_pNetworkLayer (0),
-	m_pARPHandler (0),
-	m_nRawProtocolType (0)
+:   m_pNetConfig (pNetConfig),
+    m_pNetDevLayer (pNetDevLayer),
+    m_pNetworkLayer (0),
+    m_pARPHandler (0),
+    m_nRawProtocolType (0)
 {
-	assert (m_pNetConfig != 0);
-	assert (m_pNetDevLayer != 0);
+    assert (m_pNetConfig != 0);
+    assert (m_pNetDevLayer != 0);
 }
 
 CLinkLayer::~CLinkLayer (void)
 {
-	delete m_pARPHandler;
-	m_pARPHandler = 0;
+    delete m_pARPHandler;
+    m_pARPHandler = 0;
 
-	m_pNetworkLayer = 0;
-	m_pNetDevLayer = 0;
-	m_pNetConfig = 0;
+    m_pNetworkLayer = 0;
+    m_pNetDevLayer = 0;
+    m_pNetConfig = 0;
 }
 
 boolean CLinkLayer::Initialize (void)
 {
-	assert (m_pNetConfig != 0);
-	m_pARPHandler = new CARPHandler (m_pNetConfig, m_pNetDevLayer, this, &m_ARPRxQueue);
-	assert (m_pARPHandler != 0);
+    assert (m_pNetConfig != 0);
+    m_pARPHandler = new CARPHandler (m_pNetConfig, m_pNetDevLayer, this, &m_ARPRxQueue);
+    assert (m_pARPHandler != 0);
 
-	return TRUE;
+    return TRUE;
 }
 
 void CLinkLayer::AttachLayer (CNetworkLayer *pNetworkLayer)
 {
-	assert (m_pNetworkLayer == 0);
-	m_pNetworkLayer = pNetworkLayer;
-	assert (m_pNetworkLayer != 0);
+    assert (m_pNetworkLayer == 0);
+    m_pNetworkLayer = pNetworkLayer;
+    assert (m_pNetworkLayer != 0);
 }
 
 void CLinkLayer::Process (void)
 {
-	assert (m_pNetDevLayer != 0);
-	const CMACAddress *pOwnMACAddress = m_pNetDevLayer->GetMACAddress ();
-	if (pOwnMACAddress == 0)
-	{
-		return;
-	}
+    assert (m_pNetDevLayer != 0);
+    const CMACAddress *pOwnMACAddress = m_pNetDevLayer->GetMACAddress ();
+    if (pOwnMACAddress == 0)
+    {
+        return;
+    }
 
-	assert (m_pNetDevLayer != 0);
-	u8 Buffer[FRAME_BUFFER_SIZE];
-	unsigned nLength;
-	while (m_pNetDevLayer->Receive (Buffer, &nLength))
-	{
-		assert (nLength <= FRAME_BUFFER_SIZE);
-		if (nLength <= sizeof (TEthernetHeader))
-		{
-			continue;
-		}
-		TEthernetHeader *pHeader = (TEthernetHeader *) Buffer;
+    assert (m_pNetDevLayer != 0);
+    u8 Buffer[FRAME_BUFFER_SIZE];
+    unsigned nLength;
+    while (m_pNetDevLayer->Receive (Buffer, &nLength))
+    {
+        assert (nLength <= FRAME_BUFFER_SIZE);
+        if (nLength <= sizeof (TEthernetHeader))
+        {
+            continue;
+        }
+        TEthernetHeader *pHeader = (TEthernetHeader *) Buffer;
 
-		CMACAddress MACAddressReceiver (pHeader->MACReceiver);
-		if (    MACAddressReceiver != *pOwnMACAddress
-		    && !MACAddressReceiver.IsBroadcast ())
-		{
-			continue;
-		}
+        CMACAddress MACAddressReceiver (pHeader->MACReceiver);
+        if (    MACAddressReceiver != *pOwnMACAddress
+            && !MACAddressReceiver.IsBroadcast ())
+        {
+            continue;
+        }
 
-		nLength -= sizeof (TEthernetHeader);
-		assert (nLength > 0);
-		
-		switch (pHeader->nProtocolType)
-		{
-		case BE (ETH_PROT_IP):
-			m_IPRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader), nLength);
-			break;
+        nLength -= sizeof (TEthernetHeader);
+        assert (nLength > 0);
 
-		case BE (ETH_PROT_ARP):
-			m_ARPRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader), nLength);
-			break;
+        switch (pHeader->nProtocolType)
+        {
+        case BE (ETH_PROT_IP):
+            m_IPRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader), nLength);
+            break;
 
-		default:
-			if (pHeader->nProtocolType == m_nRawProtocolType)
-			{
-				TRawPrivateData *pParam = new TRawPrivateData;
-				assert (pParam != 0);
-				memcpy (pParam->MACSender, pHeader->MACSender, MAC_ADDRESS_SIZE);
+        case BE (ETH_PROT_ARP):
+            m_ARPRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader), nLength);
+            break;
 
-				m_RawRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader),
-						      nLength, pParam);
-			}
-			break;
-		}
-	}
+        default:
+            if (pHeader->nProtocolType == m_nRawProtocolType)
+            {
+                TRawPrivateData *pParam = new TRawPrivateData;
+                assert (pParam != 0);
+                memcpy (pParam->MACSender, pHeader->MACSender, MAC_ADDRESS_SIZE);
 
-	assert (m_pARPHandler != 0);
-	m_pARPHandler->Process ();
+                m_RawRxQueue.Enqueue (Buffer+sizeof (TEthernetHeader),
+                              nLength, pParam);
+            }
+            break;
+        }
+    }
+
+    assert (m_pARPHandler != 0);
+    m_pARPHandler->Process ();
 }
 
 boolean CLinkLayer::Send (const CIPAddress &rReceiver, const void *pIPPacket, unsigned nLength)
 {
-	unsigned nFrameLength = sizeof (TEthernetHeader) + nLength;	// may wrap
-	if (   nFrameLength <= sizeof (TEthernetHeader)
-	    || nFrameLength > FRAME_BUFFER_SIZE)
-	{
-		return FALSE;
-	}
+    unsigned nFrameLength = sizeof (TEthernetHeader) + nLength;    // may wrap
+    if (   nFrameLength <= sizeof (TEthernetHeader)
+        || nFrameLength > FRAME_BUFFER_SIZE)
+    {
+        return FALSE;
+    }
 
-	u8 FrameBuffer[nFrameLength];
-	TEthernetHeader *pHeader = (TEthernetHeader *) FrameBuffer;
+    u8 FrameBuffer[nFrameLength];
+    TEthernetHeader *pHeader = (TEthernetHeader *) FrameBuffer;
 
-	assert (m_pNetDevLayer != 0);
-	const CMACAddress *pOwnMACAddress = m_pNetDevLayer->GetMACAddress ();
-	assert (pOwnMACAddress != 0);
-	pOwnMACAddress->CopyTo (pHeader->MACSender);
+    assert (m_pNetDevLayer != 0);
+    const CMACAddress *pOwnMACAddress = m_pNetDevLayer->GetMACAddress ();
+    assert (pOwnMACAddress != 0);
+    pOwnMACAddress->CopyTo (pHeader->MACSender);
 
-	pHeader->nProtocolType = BE (ETH_PROT_IP);
+    pHeader->nProtocolType = BE (ETH_PROT_IP);
 
-	assert (pIPPacket != 0);
-	assert (nLength > 0);
-	memcpy (FrameBuffer+sizeof (TEthernetHeader), pIPPacket, nLength);
+    assert (pIPPacket != 0);
+    assert (nLength > 0);
+    memcpy (FrameBuffer+sizeof (TEthernetHeader), pIPPacket, nLength);
 
-	assert (m_pNetConfig != 0);
-	assert (m_pARPHandler != 0);
-	CMACAddress MACAddressReceiver;
-	if (   rReceiver.IsBroadcast ()
-	    || rReceiver == *m_pNetConfig->GetBroadcastAddress ())
-	{
-		MACAddressReceiver.SetBroadcast ();
-	}
-	else if (!m_pARPHandler->Resolve (rReceiver, &MACAddressReceiver,
-					  FrameBuffer, nFrameLength))
-	{
-		return TRUE;		// packet will be retransmitted by ARP handler
-	}
+    assert (m_pNetConfig != 0);
+    assert (m_pARPHandler != 0);
+    CMACAddress MACAddressReceiver;
+    if (   rReceiver.IsBroadcast ()
+        || rReceiver == *m_pNetConfig->GetBroadcastAddress ())
+    {
+        MACAddressReceiver.SetBroadcast ();
+    }
+    else if (!m_pARPHandler->Resolve (rReceiver, &MACAddressReceiver,
+                      FrameBuffer, nFrameLength))
+    {
+        return TRUE;        // packet will be retransmitted by ARP handler
+    }
 
-	MACAddressReceiver.CopyTo (pHeader->MACReceiver);
+    MACAddressReceiver.CopyTo (pHeader->MACReceiver);
 
-	m_pNetDevLayer->Send (FrameBuffer, nFrameLength);
+    m_pNetDevLayer->Send (FrameBuffer, nFrameLength);
 
-	return TRUE;
+    return TRUE;
 }
 
 boolean CLinkLayer::Receive (void *pBuffer, unsigned *pResultLength)
 {
-	assert (pBuffer != 0);
-	assert (pResultLength != 0);
-	*pResultLength = m_IPRxQueue.Dequeue (pBuffer);
+    assert (pBuffer != 0);
+    assert (pResultLength != 0);
+    *pResultLength = m_IPRxQueue.Dequeue (pBuffer);
 
-	return *pResultLength != 0 ? TRUE : FALSE;
+    return *pResultLength != 0 ? TRUE : FALSE;
 }
 
 boolean CLinkLayer::SendRaw (const void *pFrame, unsigned nLength)
 {
-	assert (pFrame != 0);
-	assert (nLength > 0);
-	assert (m_pNetDevLayer != 0);
-	m_pNetDevLayer->Send (pFrame, nLength);
+    assert (pFrame != 0);
+    assert (nLength > 0);
+    assert (m_pNetDevLayer != 0);
+    m_pNetDevLayer->Send (pFrame, nLength);
 
-	return TRUE;
+    return TRUE;
 }
 
 boolean CLinkLayer::ReceiveRaw (void *pBuffer, unsigned *pResultLength, CMACAddress *pSender)
 {
-	void *pParam;
-	assert (pBuffer != 0);
-	assert (pResultLength != 0);
-	*pResultLength = m_RawRxQueue.Dequeue (pBuffer, &pParam);
-	if (*pResultLength == 0)
-	{
-		return FALSE;
-	}
+    void *pParam;
+    assert (pBuffer != 0);
+    assert (pResultLength != 0);
+    *pResultLength = m_RawRxQueue.Dequeue (pBuffer, &pParam);
+    if (*pResultLength == 0)
+    {
+        return FALSE;
+    }
 
-	TRawPrivateData *pData = (TRawPrivateData *) pParam;
+    TRawPrivateData *pData = (TRawPrivateData *) pParam;
 
-	if (pSender != 0)
-	{
-		assert (pData != 0);
-		pSender->Set (pData->MACSender);
-	}
+    if (pSender != 0)
+    {
+        assert (pData != 0);
+        pSender->Set (pData->MACSender);
+    }
 
-	delete pData;
+    delete pData;
 
-	return TRUE;
+    return TRUE;
 }
 
 boolean CLinkLayer::EnableReceiveRaw (u16 nProtocolType)
 {
-	if (m_nRawProtocolType != 0)
-	{
-		return FALSE;
-	}
+    if (m_nRawProtocolType != 0)
+    {
+        return FALSE;
+    }
 
-	assert (nProtocolType != 0);
-	m_nRawProtocolType = le2be16 (nProtocolType);
+    assert (nProtocolType != 0);
+    m_nRawProtocolType = le2be16 (nProtocolType);
 
-	return TRUE;
+    return TRUE;
 }
 
 void CLinkLayer::ResolveFailed (const void *pReturnedFrame, unsigned nLength)
 {
-	assert (pReturnedFrame != 0);
-	assert (nLength > sizeof (TEthernetHeader));
-	assert (m_pNetworkLayer != 0);
+    assert (pReturnedFrame != 0);
+    assert (nLength > sizeof (TEthernetHeader));
+    assert (m_pNetworkLayer != 0);
 
-	m_pNetworkLayer->SendFailed (ICMP_CODE_DEST_HOST_UNREACH,
-				     (const u8 *) pReturnedFrame + sizeof (TEthernetHeader),
-				     nLength - sizeof (TEthernetHeader));
+    m_pNetworkLayer->SendFailed (ICMP_CODE_DEST_HOST_UNREACH,
+                     (const u8 *) pReturnedFrame + sizeof (TEthernetHeader),
+                     nLength - sizeof (TEthernetHeader));
 }
