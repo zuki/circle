c6778d377e56e60026d7111b37c271b7181dc58b
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 15:03:21 +0900

    koptionsのコードのドキュメント付け

--- include/circle/koptions.h
+++ include/circle/koptions.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2022  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -16,7 +16,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 #ifndef _circle_koptions_h
 #define _circle_koptions_h
 
@@ -24,74 +24,112 @@
 #include <circle/cputhrottle.h>
 #include <circle/types.h>
 
+/**
+ * @class CKernelOptions
+ * @brief カーネルオプション
+ */
 class CKernelOptions
 {
 public:
-	CKernelOptions (void);
-	~CKernelOptions (void);
+    /// @brief コンストラクタ
+    CKernelOptions (void);
+    /// @brief デストラクタ
+    ~CKernelOptions (void);
 
-	unsigned GetWidth (void) const;
-	unsigned GetHeight (void) const;
-
-	const char *GetLogDevice (void) const;
-	unsigned GetLogLevel (void) const;
-
-	const char *GetKeyMap (void) const;
-
-	unsigned GetUSBPowerDelay (void) const;
-	boolean GetUSBFullSpeed (void) const;
-	boolean GetUSBBoost (void) const;
-	const char *GetUSBIgnore (void) const;		// defaults to empty string
-
-	const char *GetSoundDevice (void) const;	// defaults to empty string
-	unsigned GetSoundOption (void) const;
-
-	TCPUSpeed GetCPUSpeed (void) const;
-	unsigned GetSoCMaxTemp (void) const;
-	unsigned GetGPIOFanPin (void) const;		// returns 0, if not defined
-
-	const unsigned *GetTouchScreen (void) const;	// returns 4 values (nullptr if unset)
-
-	static CKernelOptions *Get (void);
+    //! Getter: 画面幅
+    unsigned GetWidth (void) const;
+    //! Getter: 画面高さ
+    unsigned GetHeight (void) const;
+    //! Getter: ログデバイス
+    const char *GetLogDevice (void) const;
+    //! Getter: ログ出力レベル
+    unsigned GetLogLevel (void) const;
+    //! Getter: キーマップ
+    const char *GetKeyMap (void) const;
+    //! Getter: USB電源遅延時間
+    unsigned GetUSBPowerDelay (void) const;
+    //! Getter: USBフルスピードか
+    boolean GetUSBFullSpeed (void) const;
+    //! Getter: USBブースト
+    boolean GetUSBBoost (void) const;
+    //! Getter: 無視するUSBインタフェース
+    const char *GetUSBIgnore (void) const;          // defaults to empty string
+    //! Getter: サウンドデバイス
+    const char *GetSoundDevice (void) const;        // defaults to empty string
+    //! Getter: サウンドオプション
+    unsigned GetSoundOption (void) const;
+    //! Getter: CPUスピード
+    TCPUSpeed GetCPUSpeed (void) const;
+    //! Getter: Soc最大温度
+    unsigned GetSoCMaxTemp (void) const;
+    //! Getter: GPIOファンピン
+    unsigned GetGPIOFanPin (void) const;            // returns 0, if not defined
+    //! Getter: タッチスクリーン
+    const unsigned *GetTouchScreen (void) const;    // returns 4 values (nullptr if unset)
+    //! Getter: オブジェクト自身
+    static CKernelOptions *Get (void);
 
 private:
-	char *GetToken (void);				// returns next "option=value" pair, 0 if nothing follows
+    /// @brief 次の"option=value"ペアを返す
+    /// @return 次の"option=value"ペア、ない場合は0
+    char *GetToken (void);
+    /// @brief "option=value"ペアの値を返す
+    /// @param pOption "option=value"ペア
+    /// @return '\0'終端のオプション値
+    static char *GetOptionValue (char *pOption);
+    /// @brief 10進数文字列を数値に変換する
+    /// @param pString 文字列
+    /// @return 10進数値、エラーの場合は -1
+    static unsigned GetDecimal (char *pString);
 
-	static char *GetOptionValue (char *pOption);	// returns value and terminates option with '\0'
+    // fetches nCount comma-separated decimals from pString to pResult
 
-	static unsigned GetDecimal (char *pString);	// returns decimal value, -1 on error
-
-	// fetches nCount comma-separated decimals from pString to pResult
-	static boolean GetDecimals (char *pString, unsigned *pResult, unsigned nCount);
+    /// @brief カンマ区切りの数値文字列から最大nCountの数値を取り出す
+    /// @param pString 数値文字列
+    /// @param pResult 変換後の数値
+    /// @param nCount 最大変換数
+    /// @return nCount個あったらTRUE, まだ変換されていない数値があったらFALSE
+    static boolean GetDecimals (char *pString, unsigned *pResult, unsigned nCount);
 
 private:
-	TPropertyTagCommandLine m_TagCommandLine;
-	char *m_pOptions;
-
-	unsigned m_nWidth;
-	unsigned m_nHeight;
-
-	char m_LogDevice[20];
-	unsigned m_nLogLevel;
-
-	char m_KeyMap[3];
-
-	unsigned m_nUSBPowerDelay;
-	boolean m_bUSBFullSpeed;
-	boolean m_bUSBBoost;
-	char m_USBIgnore[20];
-
-	char m_SoundDevice[20];
-	unsigned m_nSoundOption;
-
-	TCPUSpeed m_CPUSpeed;
-	unsigned m_nSoCMaxTemp;
-	unsigned m_nGPIOFanPin;
-
-	boolean m_bTouchScreenValid;
-	unsigned m_TouchScreen[4];
-
-	static CKernelOptions *s_pThis;
+    /// @brief コマンドライン取得のためのMailBox属性
+    TPropertyTagCommandLine m_TagCommandLine;
+    /// @brief コマンドラインオプション文字列
+    char *m_pOptions;
+    /// @brief 画面幅
+    unsigned m_nWidth;
+    /// @brief 画面高さ
+    unsigned m_nHeight;
+    /// @brief ログデバイス
+    char m_LogDevice[20];
+    /// @brief ログレベル
+    unsigned m_nLogLevel;
+    /// @brief キーマップ
+    char m_KeyMap[3];
+    /// @brief USB電源遅延
+    unsigned m_nUSBPowerDelay;
+    /// @brief USBフルスピードか
+    boolean m_bUSBFullSpeed;
+    /// @brief USBブースト
+    boolean m_bUSBBoost;
+    /// @brief 無視するUSBインタフェース
+    char m_USBIgnore[20];
+    /// @brief サウンドデバイス
+    char m_SoundDevice[20];
+    /// @brief サウンドオプション
+    unsigned m_nSoundOption;
+    /// @brief CPUスピード
+    TCPUSpeed m_CPUSpeed;
+    /// @brief SoC最大温度
+    unsigned m_nSoCMaxTemp;
+    /// @brief GPIOファンピン
+    unsigned m_nGPIOFanPin;
+    /// @brief タッチスクリーンあり
+    boolean m_bTouchScreenValid;
+    /// @brief タッチスクリーン
+    unsigned m_TouchScreen[4];
+    /// @brief このオブジェクト
+    static CKernelOptions *s_pThis;
 };
 
 #endif
--- lib/koptions.cpp
+++ lib/koptions.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2022  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -16,359 +16,359 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 #include <circle/koptions.h>
 #include <circle/logger.h>
 #include <circle/util.h>
 #include <circle/sysconfig.h>
 
-#define INVALID_VALUE	((unsigned) -1)
+#define INVALID_VALUE    ((unsigned) -1)
 
 CKernelOptions *CKernelOptions::s_pThis = 0;
 
 CKernelOptions::CKernelOptions (void)
-:	m_nWidth (0),
-	m_nHeight (0),
-	m_nLogLevel (LogDebug),
-	m_nUSBPowerDelay (0),
-	m_bUSBFullSpeed (FALSE),
-	m_bUSBBoost (FALSE),
-	m_nSoundOption (0),
-	m_CPUSpeed (CPUSpeedLow),
-	m_nSoCMaxTemp (60),
-	m_nGPIOFanPin (0),
-	m_bTouchScreenValid (FALSE)
+:   m_nWidth (0),
+    m_nHeight (0),
+    m_nLogLevel (LogDebug),
+    m_nUSBPowerDelay (0),
+    m_bUSBFullSpeed (FALSE),
+    m_bUSBBoost (FALSE),
+    m_nSoundOption (0),
+    m_CPUSpeed (CPUSpeedLow),
+    m_nSoCMaxTemp (60),
+    m_nGPIOFanPin (0),
+    m_bTouchScreenValid (FALSE)
 {
-	strcpy (m_LogDevice, "tty1");
-	strcpy (m_KeyMap, DEFAULT_KEYMAP);
-	m_USBIgnore[0] = '\0';
-	m_SoundDevice[0] = '\0';
+    strcpy (m_LogDevice, "tty1");
+    strcpy (m_KeyMap, DEFAULT_KEYMAP);
+    m_USBIgnore[0] = '\0';
+    m_SoundDevice[0] = '\0';
 
-	s_pThis = this;
+    s_pThis = this;
 
-	CBcmPropertyTags Tags;
-	if (!Tags.GetTag (PROPTAG_GET_COMMAND_LINE, &m_TagCommandLine, sizeof m_TagCommandLine))
-	{
-		return;
-	}
+    CBcmPropertyTags Tags;
+    if (!Tags.GetTag (PROPTAG_GET_COMMAND_LINE, &m_TagCommandLine, sizeof m_TagCommandLine))
+    {
+        return;
+    }
 
-	if (m_TagCommandLine.Tag.nValueLength >= sizeof m_TagCommandLine.String)
-	{
-		return;
-	}
-	m_TagCommandLine.String[m_TagCommandLine.Tag.nValueLength] = '\0';
-	
-	m_pOptions = (char *) m_TagCommandLine.String;
+    if (m_TagCommandLine.Tag.nValueLength >= sizeof m_TagCommandLine.String)
+    {
+        return;
+    }
+    m_TagCommandLine.String[m_TagCommandLine.Tag.nValueLength] = '\0';
 
-	char *pOption;
-	while ((pOption = GetToken ()) != 0)
-	{
-		char *pValue = GetOptionValue (pOption);
+    m_pOptions = (char *) m_TagCommandLine.String;
 
-		if (strcmp (pOption, "width") == 0)
-		{
-			unsigned nValue;
-			if ((nValue = GetDecimal (pValue)) != INVALID_VALUE)
-			{
-				m_nWidth = nValue;
-			}
-		}
-		else if (strcmp (pOption, "height") == 0)
-		{
-			unsigned nValue;
-			if ((nValue = GetDecimal (pValue)) != INVALID_VALUE)
-			{
-				m_nHeight = nValue;
-			}
-		}
-		else if (strcmp (pOption, "logdev") == 0)
-		{
-			strncpy (m_LogDevice, pValue, sizeof m_LogDevice-1);
-			m_LogDevice[sizeof m_LogDevice-1] = '\0';
-		}
-		else if (strcmp (pOption, "loglevel") == 0)
-		{
-			unsigned nValue;
-			if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
-			    && nValue <= LogDebug)
-			{
-				m_nLogLevel = nValue;
-			}
-		}
-		else if (strcmp (pOption, "keymap") == 0)
-		{
-			strncpy (m_KeyMap, pValue, sizeof m_KeyMap-1);
-			m_KeyMap[sizeof m_KeyMap-1] = '\0';
-		}
-		else if (strcmp (pOption, "usbpowerdelay") == 0)
-		{
-			unsigned nValue;
-			if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
-			    && 200 <= nValue && nValue <= 8000)
-			{
-				m_nUSBPowerDelay = nValue;
-			}
-		}
-		else if (strcmp (pOption, "usbspeed") == 0)
-		{
-			if (strcmp (pValue, "full") == 0)
-			{
-				m_bUSBFullSpeed = TRUE;
-			}
-		}
-		else if (strcmp (pOption, "usbboost") == 0)
-		{
-			if (strcmp (pValue, "true") == 0)
-			{
-				m_bUSBBoost = TRUE;
-			}
-		}
-		else if (strcmp (pOption, "usbignore") == 0)
-		{
-			strncpy (m_USBIgnore, pValue, sizeof m_USBIgnore-1);
-			m_USBIgnore[sizeof m_USBIgnore-1] = '\0';
-		}
-		else if (strcmp (pOption, "sounddev") == 0)
-		{
-			strncpy (m_SoundDevice, pValue, sizeof m_SoundDevice-1);
-			m_SoundDevice[sizeof m_SoundDevice-1] = '\0';
-		}
-		else if (strcmp (pOption, "soundopt") == 0)
-		{
-			unsigned nValue;
-			if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
-			    && nValue <= 2)
-			{
-				m_nSoundOption = nValue;
-			}
-		}
-		else if (strcmp (pOption, "fast") == 0)
-		{
-			if (strcmp (pValue, "true") == 0)
-			{
-				m_CPUSpeed = CPUSpeedMaximum;
-			}
-		}
-		else if (strcmp (pOption, "socmaxtemp") == 0)
-		{
-			unsigned nValue;
-			if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
-			    && 40 <= nValue && nValue <= 78)
-			{
-				m_nSoCMaxTemp = nValue;
-			}
-		}
-		else if (strcmp (pOption, "gpiofanpin") == 0)
-		{
-			unsigned nValue;
-			if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
-			    && 2 <= nValue && nValue <= 27)
-			{
-				m_nGPIOFanPin = nValue;
-			}
-		}
-		else if (strcmp (pOption, "touchscreen") == 0)
-		{
-			m_bTouchScreenValid = GetDecimals (pValue, m_TouchScreen, 4);
-		}
-	}
+    char *pOption;
+    while ((pOption = GetToken ()) != 0)
+    {
+        char *pValue = GetOptionValue (pOption);
+
+        if (strcmp (pOption, "width") == 0)
+        {
+            unsigned nValue;
+            if ((nValue = GetDecimal (pValue)) != INVALID_VALUE)
+            {
+                m_nWidth = nValue;
+            }
+        }
+        else if (strcmp (pOption, "height") == 0)
+        {
+            unsigned nValue;
+            if ((nValue = GetDecimal (pValue)) != INVALID_VALUE)
+            {
+                m_nHeight = nValue;
+            }
+        }
+        else if (strcmp (pOption, "logdev") == 0)
+        {
+            strncpy (m_LogDevice, pValue, sizeof m_LogDevice-1);
+            m_LogDevice[sizeof m_LogDevice-1] = '\0';
+        }
+        else if (strcmp (pOption, "loglevel") == 0)
+        {
+            unsigned nValue;
+            if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
+                && nValue <= LogDebug)
+            {
+                m_nLogLevel = nValue;
+            }
+        }
+        else if (strcmp (pOption, "keymap") == 0)
+        {
+            strncpy (m_KeyMap, pValue, sizeof m_KeyMap-1);
+            m_KeyMap[sizeof m_KeyMap-1] = '\0';
+        }
+        else if (strcmp (pOption, "usbpowerdelay") == 0)
+        {
+            unsigned nValue;
+            if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
+                && 200 <= nValue && nValue <= 8000)
+            {
+                m_nUSBPowerDelay = nValue;
+            }
+        }
+        else if (strcmp (pOption, "usbspeed") == 0)
+        {
+            if (strcmp (pValue, "full") == 0)
+            {
+                m_bUSBFullSpeed = TRUE;
+            }
+        }
+        else if (strcmp (pOption, "usbboost") == 0)
+        {
+            if (strcmp (pValue, "true") == 0)
+            {
+                m_bUSBBoost = TRUE;
+            }
+        }
+        else if (strcmp (pOption, "usbignore") == 0)
+        {
+            strncpy (m_USBIgnore, pValue, sizeof m_USBIgnore-1);
+            m_USBIgnore[sizeof m_USBIgnore-1] = '\0';
+        }
+        else if (strcmp (pOption, "sounddev") == 0)
+        {
+            strncpy (m_SoundDevice, pValue, sizeof m_SoundDevice-1);
+            m_SoundDevice[sizeof m_SoundDevice-1] = '\0';
+        }
+        else if (strcmp (pOption, "soundopt") == 0)
+        {
+            unsigned nValue;
+            if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
+                && nValue <= 2)
+            {
+                m_nSoundOption = nValue;
+            }
+        }
+        else if (strcmp (pOption, "fast") == 0)
+        {
+            if (strcmp (pValue, "true") == 0)
+            {
+                m_CPUSpeed = CPUSpeedMaximum;
+            }
+        }
+        else if (strcmp (pOption, "socmaxtemp") == 0)
+        {
+            unsigned nValue;
+            if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
+                && 40 <= nValue && nValue <= 78)
+            {
+                m_nSoCMaxTemp = nValue;
+            }
+        }
+        else if (strcmp (pOption, "gpiofanpin") == 0)
+        {
+            unsigned nValue;
+            if (   (nValue = GetDecimal (pValue)) != INVALID_VALUE
+                && 2 <= nValue && nValue <= 27)
+            {
+                m_nGPIOFanPin = nValue;
+            }
+        }
+        else if (strcmp (pOption, "touchscreen") == 0)
+        {
+            m_bTouchScreenValid = GetDecimals (pValue, m_TouchScreen, 4);
+        }
+    }
 }
 
 CKernelOptions::~CKernelOptions (void)
 {
-	s_pThis = 0;
+    s_pThis = 0;
 }
 
 unsigned CKernelOptions::GetWidth (void) const
 {
-	return m_nWidth;
+    return m_nWidth;
 }
 
 unsigned CKernelOptions::GetHeight (void) const
 {
-	return m_nHeight;
+    return m_nHeight;
 }
 
 const char *CKernelOptions::GetLogDevice (void) const
 {
-	return m_LogDevice;
+    return m_LogDevice;
 }
 
 unsigned CKernelOptions::GetLogLevel (void) const
 {
-	return m_nLogLevel;
+    return m_nLogLevel;
 }
 
 const char *CKernelOptions::GetKeyMap (void) const
 {
-	return m_KeyMap;
+    return m_KeyMap;
 }
 
 unsigned CKernelOptions::GetUSBPowerDelay (void) const
 {
-	return m_nUSBPowerDelay;
+    return m_nUSBPowerDelay;
 }
 
 boolean CKernelOptions::GetUSBFullSpeed (void) const
 {
-	return m_bUSBFullSpeed;
+    return m_bUSBFullSpeed;
 }
 
 boolean CKernelOptions::GetUSBBoost (void) const
 {
-	return m_bUSBBoost;
+    return m_bUSBBoost;
 }
 
 const char *CKernelOptions::GetUSBIgnore (void) const
 {
-	return m_USBIgnore;
+    return m_USBIgnore;
 }
 
 const char *CKernelOptions::GetSoundDevice (void) const
 {
-	return m_SoundDevice;
+    return m_SoundDevice;
 }
 
 unsigned CKernelOptions::GetSoundOption (void) const
 {
-	return m_nSoundOption;
+    return m_nSoundOption;
 }
 
 TCPUSpeed CKernelOptions::GetCPUSpeed (void) const
 {
-	return m_CPUSpeed;
+    return m_CPUSpeed;
 }
 
 unsigned CKernelOptions::GetSoCMaxTemp (void) const
 {
-	return m_nSoCMaxTemp;
+    return m_nSoCMaxTemp;
 }
 
 unsigned CKernelOptions::GetGPIOFanPin (void) const
 {
-	return m_nGPIOFanPin;
+    return m_nGPIOFanPin;
 }
 
 const unsigned *CKernelOptions::GetTouchScreen (void) const
 {
-	return m_bTouchScreenValid ? m_TouchScreen : nullptr;
+    return m_bTouchScreenValid ? m_TouchScreen : nullptr;
 }
 
 CKernelOptions *CKernelOptions::Get (void)
 {
-	return s_pThis;
+    return s_pThis;
 }
 
 char *CKernelOptions::GetToken (void)
 {
-	while (*m_pOptions != '\0')
-	{
-		if (*m_pOptions != ' ')
-		{
-			break;
-		}
+    while (*m_pOptions != '\0')
+    {
+        if (*m_pOptions != ' ')
+        {
+            break;
+        }
 
-		m_pOptions++;
-	}
+        m_pOptions++;
+    }
 
-	if (*m_pOptions == '\0')
-	{
-		return 0;
-	}
+    if (*m_pOptions == '\0')
+    {
+        return 0;
+    }
 
-	char *pToken = m_pOptions;
+    char *pToken = m_pOptions;
 
-	while (*m_pOptions != '\0')
-	{
-		if (*m_pOptions == ' ')
-		{
-			*m_pOptions++ = '\0';
+    while (*m_pOptions != '\0')
+    {
+        if (*m_pOptions == ' ')
+        {
+            *m_pOptions++ = '\0';
 
-			break;
-		}
+            break;
+        }
 
-		m_pOptions++;
-	}
+        m_pOptions++;
+    }
 
-	return pToken;
+    return pToken;
 }
 
 char *CKernelOptions::GetOptionValue (char *pOption)
 {
-	while (*pOption != '\0')
-	{
-		if (*pOption == '=')
-		{
-			break;
-		}
+    while (*pOption != '\0')
+    {
+        if (*pOption == '=')
+        {
+            break;
+        }
 
-		pOption++;
-	}
+        pOption++;
+    }
 
-	if (*pOption == '\0')
-	{
-		return 0;
-	}
+    if (*pOption == '\0')
+    {
+        return 0;
+    }
 
-	*pOption++ = '\0';
+    *pOption++ = '\0';
 
-	return pOption;
+    return pOption;
 }
 
 unsigned CKernelOptions::GetDecimal (char *pString)
 {
-	if (   pString == 0
-	    || *pString == '\0')
-	{
-		return INVALID_VALUE;
-	}
+    if (   pString == 0
+        || *pString == '\0')
+    {
+        return INVALID_VALUE;
+    }
 
-	unsigned nResult = 0;
+    unsigned nResult = 0;
 
-	char chChar;
-	while ((chChar = *pString++) != '\0')
-	{
-		if (!('0' <= chChar && chChar <= '9'))
-		{
-			return INVALID_VALUE;
-		}
+    char chChar;
+    while ((chChar = *pString++) != '\0')
+    {
+        if (!('0' <= chChar && chChar <= '9'))
+        {
+            return INVALID_VALUE;
+        }
 
-		unsigned nPrevResult = nResult;
+        unsigned nPrevResult = nResult;
 
-		nResult = nResult * 10 + (chChar - '0');
-		if (   nResult < nPrevResult
-		    || nResult == INVALID_VALUE)
-		{
-			return INVALID_VALUE;
-		}
-	}
+        nResult = nResult * 10 + (chChar - '0');
+        if (   nResult < nPrevResult
+            || nResult == INVALID_VALUE)
+        {
+            return INVALID_VALUE;
+        }
+    }
 
-	return nResult;
+    return nResult;
 }
 
 boolean CKernelOptions::GetDecimals (char *pString, unsigned *pResult, unsigned nCount)
 {
-	static const char Delim[] = ",";
+    static const char Delim[] = ",";
 
-	char *pSavePtr;
-	while (nCount--)
-	{
-		char *pToken = strtok_r (pString, Delim, &pSavePtr);
-		if (!pToken)
-		{
-			return FALSE;
-		}
+    char *pSavePtr;
+    while (nCount--)
+    {
+        char *pToken = strtok_r (pString, Delim, &pSavePtr);
+        if (!pToken)
+        {
+            return FALSE;
+        }
 
-		unsigned nValue = GetDecimal (pToken);
-		if (nValue == INVALID_VALUE)
-		{
-			return FALSE;
-		}
+        unsigned nValue = GetDecimal (pToken);
+        if (nValue == INVALID_VALUE)
+        {
+            return FALSE;
+        }
 
-		*pResult++ = nValue;
+        *pResult++ = nValue;
 
-		pString = nullptr;
-	}
+        pString = nullptr;
+    }
 
-	return !strtok_r (nullptr, Delim, &pSavePtr);
+    return !strtok_r (nullptr, Delim, &pSavePtr);
 }
