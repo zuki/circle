4eb2a1a6d901bd3179e0b24e74de1aaa6c7aa04e
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 15:07:46 +0900

    net/netsubsystemのコードのドキュメント付け

--- include/circle/net/netsubsystem.h
+++ include/circle/net/netsubsystem.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -28,47 +28,77 @@
 #include <circle/string.h>
 #include <circle/types.h>
 
-#define DEFAULT_HOSTNAME	"raspberrypi"
+#define DEFAULT_HOSTNAME    "raspberrypi"
 
 class CDHCPClient;
 
+/// @brief ネットサブシステムクラス
 class CNetSubSystem
 {
 public:
-	CNetSubSystem (const u8 *pIPAddress      = 0,	// use DHCP if pIPAddress == 0
-		       const u8 *pNetMask        = 0,
-		       const u8 *pDefaultGateway = 0,
-		       const u8 *pDNSServer      = 0,
-		       const char *pHostname	 = DEFAULT_HOSTNAME,	// 0 for no hostname
-		       TNetDeviceType DeviceType = NetDeviceTypeEthernet);
-	~CNetSubSystem (void);
-	
-	boolean Initialize (boolean bWaitForActivate = TRUE);
+    /// @brief コンストラクタ
+    /// @param pIPAddress IPアドレス（pIPAddress == 0の場合はDHCPを使用）
+    /// @param pNetMask サブネットマスク
+    /// @param pDefaultGateway デフォルトゲートウェイ
+    /// @param pDNSServer DNSサーバ
+    /// @param pHostname ホスト名（ホスト名がない場合は0）
+    /// @param DeviceType デバイスタイプ（デフォルトはEthernet）
+    CNetSubSystem (
+            const u8    *pIPAddress         = 0,
+            const u8    *pNetMask           = 0,
+            const u8    *pDefaultGateway    = 0,
+            const u8    *pDNSServer         = 0,
+            const char  *pHostname          = DEFAULT_HOSTNAME,
+            TNetDeviceType DeviceType       = NetDeviceTypeEthernet);
+    /// @brief デストラクタ
+    ~CNetSubSystem (void);
+    /// @brief 初期化関数
+    /// @param bWaitForActivate ネットワークのが立ち上がるまで待つか
+    /// @return 処理の成否
+    boolean Initialize (boolean bWaitForActivate = TRUE);
+    /// @brief 処理を行う
+    void Process (void);
 
-	void Process (void);
+    // getter
 
-	CNetConfig *GetConfig (void);
-	CNetDeviceLayer *GetNetDeviceLayer (void);
-	CLinkLayer *GetLinkLayer (void);
-	CTransportLayer *GetTransportLayer (void);
-
-	boolean IsRunning (void) const;			// is DHCP bound if used?
-
-	static CNetSubSystem *Get (void);
+    /// @brief ネットワーク構成オブジェクトを取得
+    /// @return ネットワーク構成オブジェクトへのポインタ
+    CNetConfig *GetConfig (void);
+    /// @brief 物理層のオブジェクトを取得
+    /// @return 物理層のオブジェクトへのポインタ
+    CNetDeviceLayer *GetNetDeviceLayer (void);
+    /// @brief リンク層のオブジェクトを取得
+    /// @return ・リンク層のオブジェクトへのポインタ
+    CLinkLayer *GetLinkLayer (void);
+    /// @brief トランスポート層のオブジェクトを取得
+    /// @return トランスポート層のオブジェクトへのポインタ
+    CTransportLayer *GetTransportLayer (void);
+    /// @brief DHCPを使うのであればDHCPはバインドされているか
+    /// @return バインドの有無
+    boolean IsRunning (void) const;
+    /// @brief 自オブジェクトを取得する
+    /// @return 自オブジェクトへのポインタ
+    static CNetSubSystem *Get (void);
 
 private:
-	CString		m_Hostname;
-
-	CNetConfig	m_Config;
-	CNetDeviceLayer	m_NetDevLayer;
-	CLinkLayer	m_LinkLayer;
-	CNetworkLayer	m_NetworkLayer;
-	CTransportLayer	m_TransportLayer;
-
-	boolean		m_bUseDHCP;
-	CDHCPClient    *m_pDHCPClient;
-
-	static CNetSubSystem *s_pThis;
+    /// @brief ホスト名
+    CString         m_Hostname;
+    /// @brief ネットワーク構成のオブジェクト
+    CNetConfig      m_Config;
+    /// @brief 物理層のオブジェクト
+    CNetDeviceLayer m_NetDevLayer;
+    /// @brief リンク層のオブジェクト
+    CLinkLayer      m_LinkLayer;
+    /// @brief ネットワーク層のオブジェクト
+    CNetworkLayer   m_NetworkLayer;
+    /// @brief トランスポート層のオブジェクト
+    CTransportLayer m_TransportLayer;
+    /// @brief DSCPを使うか
+    boolean         m_bUseDHCP;
+    /// @brief DHCPクライアントオブジェクト
+    CDHCPClient    *m_pDHCPClient;
+    /// @brief 自オブジェクトへのポインタ
+    static CNetSubSystem *s_pThis;
 };
 
 #endif
--- lib/net/netsubsystem.cpp
+++ lib/net/netsubsystem.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -26,157 +26,157 @@
 CNetSubSystem *CNetSubSystem::s_pThis = 0;
 
 CNetSubSystem::CNetSubSystem (const u8 *pIPAddress, const u8 *pNetMask, const u8 *pDefaultGateway,
-			      const u8 *pDNSServer, const char *pHostname, TNetDeviceType DeviceType)
-:	m_Hostname (pHostname != 0 ? pHostname : ""),
-	m_NetDevLayer (&m_Config, DeviceType),
-	m_LinkLayer (&m_Config, &m_NetDevLayer),
-	m_NetworkLayer (&m_Config, &m_LinkLayer),
-	m_TransportLayer (&m_Config, &m_NetworkLayer),
-	m_bUseDHCP (pIPAddress == 0 ? TRUE : FALSE),
-	m_pDHCPClient (0)
+                  const u8 *pDNSServer, const char *pHostname, TNetDeviceType DeviceType)
+:   m_Hostname (pHostname != 0 ? pHostname : ""),
+    m_NetDevLayer (&m_Config, DeviceType),
+    m_LinkLayer (&m_Config, &m_NetDevLayer),
+    m_NetworkLayer (&m_Config, &m_LinkLayer),
+    m_TransportLayer (&m_Config, &m_NetworkLayer),
+    m_bUseDHCP (pIPAddress == 0 ? TRUE : FALSE),
+    m_pDHCPClient (0)
 {
-	assert (s_pThis == 0);
-	s_pThis = this;
+    assert (s_pThis == 0);
+    s_pThis = this;
+    // DHcPの使用の有無を設定
+    m_Config.SetDHCP (m_bUseDHCP);
+    // DHCPを使用しないはIPアドレス、サブネットマスク、デフォルトゲートウェイ、DNSサーバを設定
+    if (!m_bUseDHCP)
+    {
+        m_Config.SetIPAddress (pIPAddress);
+        m_Config.SetNetMask (pNetMask);
 
-	m_Config.SetDHCP (m_bUseDHCP);
+        if (pDefaultGateway != 0)
+        {
+            m_Config.SetDefaultGateway (pDefaultGateway);
+        }
 
-	if (!m_bUseDHCP)
-	{
-		m_Config.SetIPAddress (pIPAddress);
-		m_Config.SetNetMask (pNetMask);
-
-		if (pDefaultGateway != 0)
-		{
-			m_Config.SetDefaultGateway (pDefaultGateway);
-		}
-
-		if (pDNSServer != 0)
-		{
-			m_Config.SetDNSServer (pDNSServer);
-		}
-	}
+        if (pDNSServer != 0)
+        {
+            m_Config.SetDNSServer (pDNSServer);
+        }
+    }
 }
 
 CNetSubSystem::~CNetSubSystem (void)
 {
-	s_pThis = 0;
+    s_pThis = 0;
 }
 
 boolean CNetSubSystem::Initialize (boolean bWaitForActivate)
 {
-	m_bUseDHCP = m_Config.GetIPAddress ()->IsNull ();
-	m_Config.SetDHCP (m_bUseDHCP);
+    m_bUseDHCP = m_Config.GetIPAddress ()->IsNull ();
+    m_Config.SetDHCP (m_bUseDHCP);
+    // 物理層の初期化
+    if (!m_NetDevLayer.Initialize (bWaitForActivate))
+    {
+        return FALSE;
+    }
+    // リンク層の初期化
+    if (!m_LinkLayer.Initialize ())
+    {
+        return FALSE;
+    }
+    // ネットワーク層の初期化
+    if (!m_NetworkLayer.Initialize ())
+    {
+        return FALSE;
+    }
+    // リンク層にネットワーク層をアタッチ
+    m_LinkLayer.AttachLayer (&m_NetworkLayer);
+    // トランスポート層を初期化
+    if (!m_TransportLayer.Initialize ())
+    {
+        return FALSE;
+    }
+    // ネットワークタスクを作成
+    new CNetTask (this);
+    // ネットワークの立ち上げ理を待たない場合はTRUEでリターン
+    if (!bWaitForActivate)
+    {
+        return TRUE;
+    }
+    // DHCPを使用する場合はDHCPクライアントを取得してセット
+    if (m_bUseDHCP)
+    {
+        assert (m_pDHCPClient == 0);
+        m_pDHCPClient = new CDHCPClient (this, m_Hostname);
+        assert (m_pDHCPClient != 0);
+    }
 
-	if (!m_NetDevLayer.Initialize (bWaitForActivate))
-	{
-		return FALSE;
-	}
+    while (!IsRunning ())
+    {
+        CScheduler::Get ()->Yield ();
+    }
 
-	if (!m_LinkLayer.Initialize ())
-	{
-		return FALSE;
-	}
-
-	if (!m_NetworkLayer.Initialize ())
-	{
-		return FALSE;
-	}
-
-	m_LinkLayer.AttachLayer (&m_NetworkLayer);
-
-	if (!m_TransportLayer.Initialize ())
-	{
-		return FALSE;
-	}
-
-	new CNetTask (this);
-
-	if (!bWaitForActivate)
-	{
-		return TRUE;
-	}
-
-	if (m_bUseDHCP)
-	{
-		assert (m_pDHCPClient == 0);
-		m_pDHCPClient = new CDHCPClient (this, m_Hostname);
-		assert (m_pDHCPClient != 0);
-	}
-
-	while (!IsRunning ())
-	{
-		CScheduler::Get ()->Yield ();
-	}
-
-	return TRUE;
+    return TRUE;
 }
 
 void CNetSubSystem::Process (void)
 {
-	if (s_pThis == 0)
-	{
-		return;
-	}
+    if (s_pThis == 0)
+    {
+        return;
+    }
 
-	if (   m_bUseDHCP
-	    && m_pDHCPClient == 0
-	    && m_NetDevLayer.IsRunning ())
-	{
-		m_pDHCPClient = new CDHCPClient (this, m_Hostname);
-		assert (m_pDHCPClient != 0);
-	}
-
-	m_NetDevLayer.Process ();
-
-	m_LinkLayer.Process ();
-
-	m_NetworkLayer.Process ();
-
-	m_TransportLayer.Process ();
+    if (   m_bUseDHCP
+        && m_pDHCPClient == 0
+        && m_NetDevLayer.IsRunning ())
+    {
+        m_pDHCPClient = new CDHCPClient (this, m_Hostname);
+        assert (m_pDHCPClient != 0);
+    }
+    // 物理層の処理を行う
+    m_NetDevLayer.Process ();
+    // リンク層の処理を行う
+    m_LinkLayer.Process ();
+    // ネットワーク層の処理を行う
+    m_NetworkLayer.Process ();
+    // トランスポート層の処理を行う
+    m_TransportLayer.Process ();
 }
 
 CNetConfig *CNetSubSystem::GetConfig (void)
 {
-	return &m_Config;
+    return &m_Config;
 }
 
 CNetDeviceLayer *CNetSubSystem::GetNetDeviceLayer (void)
 {
-	return &m_NetDevLayer;
+    return &m_NetDevLayer;
 }
 
 CLinkLayer *CNetSubSystem::GetLinkLayer (void)
 {
-	return &m_LinkLayer;
+    return &m_LinkLayer;
 }
 
 CTransportLayer *CNetSubSystem::GetTransportLayer (void)
 {
-	return &m_TransportLayer;
+    return &m_TransportLayer;
 }
 
 boolean CNetSubSystem::IsRunning (void) const
 {
-	if (!m_NetDevLayer.IsRunning ())
-	{
-		return FALSE;
-	}
+    if (!m_NetDevLayer.IsRunning ())
+    {
+        return FALSE;
+    }
 
-	if (!m_bUseDHCP)
-	{
-		return TRUE;
-	}
+    if (!m_bUseDHCP)
+    {
+        return TRUE;
+    }
 
-	if (m_pDHCPClient == 0)
-	{
-		return FALSE;
-	}
+    if (m_pDHCPClient == 0)
+    {
+        return FALSE;
+    }
 
-	return m_pDHCPClient->IsBound ();
+    return m_pDHCPClient->IsBound ();
 }
 
 CNetSubSystem *CNetSubSystem::Get (void)
 {
-	assert (s_pThis != 0);
-	return s_pThis;
+    assert (s_pThis != 0);
+    return s_pThis;
 }
