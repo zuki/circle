9d323a879a24847da7eb158e771dbb46200ff36c
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-09-25 15:09:35 +0900

    net/ntpdaemonのコードのドキュメント付け

--- include/circle/net/ntpdaemon.h
+++ include/circle/net/ntpdaemon.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2020  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -24,21 +24,30 @@
 #include <circle/net/netsubsystem.h>
 #include <circle/string.h>
 
+/// @brief NTDデーモンクラス
 class CNTPDaemon : public CTask
 {
 public:
-	CNTPDaemon (const char	  *pNTPServer,		// Hostname
-		    CNetSubSystem *pNetSubSystem);
-	~CNTPDaemon (void);
-
-	void Run (void);
+    /// @brief コンストラクタ
+    /// @param pNTPServer NTPサーバのホスト名
+    /// @param pNetSubSystem ネットサブシステムオブジェクトへのポインタ
+    CNTPDaemon (const char  *pNTPServer,
+            CNetSubSystem   *pNetSubSystem);
+    /// @brief デストラクタ
+    ~CNTPDaemon (void);
+    /// @brief デーモン実行
+    void Run (void);
 
 private:
-	unsigned UpdateTime (void);			// returns seconds to next update attempt
+    /// @brief NTPから時刻を取得して現在痔を更新する
+    /// @return 次の更新までの秒数
+    unsigned UpdateTime (void);
 
 private:
-	CString		 m_NTPServer;
-	CNetSubSystem	*m_pNetSubSystem;
+    /// @brief NTPサーバのホスト名
+    CString         m_NTPServer;
+    /// @brief ネットサブシステム
+    CNetSubSystem  *m_pNetSubSystem;
 };
 
 #endif
--- lib/net/ntpdaemon.cpp
+++ lib/net/ntpdaemon.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2015-2021  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -29,61 +29,62 @@
 static const char FromNTPDaemon[] = "ntpd";
 
 CNTPDaemon::CNTPDaemon (const char *pNTPServer, CNetSubSystem *pNetSubSystem)
-:	m_NTPServer (pNTPServer),
-	m_pNetSubSystem (pNetSubSystem)
+:   m_NTPServer (pNTPServer),
+    m_pNetSubSystem (pNetSubSystem)
 {
-	assert (m_pNetSubSystem != 0);
+    assert (m_pNetSubSystem != 0);
 
-	SetName (FromNTPDaemon);
+    SetName (FromNTPDaemon);
 }
 
 CNTPDaemon::~CNTPDaemon (void)
 {
-	m_pNetSubSystem = 0;
+    m_pNetSubSystem = 0;
 }
 
 void CNTPDaemon::Run (void)
 {
-	while (1)
-	{
-		unsigned nSecondsToNextAttempt = UpdateTime ();
-		
-		CScheduler::Get ()->Sleep (nSecondsToNextAttempt);
-	}
+    while (1)
+    {
+        unsigned nSecondsToNextAttempt = UpdateTime ();
+
+        CScheduler::Get ()->Sleep (nSecondsToNextAttempt);
+    }
 }
 
 unsigned CNTPDaemon::UpdateTime (void)
 {
-	assert (m_pNetSubSystem != 0);
+    assert (m_pNetSubSystem != 0);
 
-	CIPAddress NTPServerIP;
-	CDNSClient DNSClient (m_pNetSubSystem);
-	if (!DNSClient.Resolve (m_NTPServer, &NTPServerIP))
-	{
-		CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot resolve: %s",
-					(const char *) m_NTPServer);
+    // DNSを引いてNTPサーバのホスト名からIPアドレスを得る
+    CIPAddress NTPServerIP;
+    CDNSClient DNSClient (m_pNetSubSystem);
+    if (!DNSClient.Resolve (m_NTPServer, &NTPServerIP))
+    {
+        CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot resolve: %s",
+                    (const char *) m_NTPServer);
 
-		return 300;
-	}
-	
-	CNTPClient NTPClient (m_pNetSubSystem);
-	unsigned nTime = NTPClient.GetTime (NTPServerIP);
-	if (nTime == 0)
-	{
-		CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot get time from %s",
-					(const char *) m_NTPServer);
+        return 300;
+    }
+    // NTPクライアントを使って現在時を取得
+    CNTPClient NTPClient (m_pNetSubSystem);
+    unsigned nTime = NTPClient.GetTime (NTPServerIP);
+    if (nTime == 0)
+    {
+        CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot get time from %s",
+                    (const char *) m_NTPServer);
 
-		return 300;
-	}
+        return 300;
+    }
+    // 現在痔を設定
+    if (CTimer::Get ()->SetTime (nTime, FALSE))
+    {
+        CLogger::Get ()->Write (FromNTPDaemon, LogNotice, "System time updated");
+    }
+    else
+    {
+        CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot update system time");
+    }
 
-	if (CTimer::Get ()->SetTime (nTime, FALSE))
-	{
-		CLogger::Get ()->Write (FromNTPDaemon, LogNotice, "System time updated");
-	}
-	else
-	{
-		CLogger::Get ()->Write (FromNTPDaemon, LogWarning, "Cannot update system time");
-	}
-
-	return 900;
+    return 900;
 }
